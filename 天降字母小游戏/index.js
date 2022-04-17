6a453bd8751a495d941999bc18124868523af9a3
amain <99476728+stimulateDxm@users.noreply.github.com>
2022-04-17 17:00:22 +0800

    Add files via upload

--- 天降字母小游戏/index.js
+++ 天降字母小游戏/index.js
@@ -8,31 +8,69 @@ let abc = "abcdefghijklnmopqrstuvwxyz"
 //记录错过的数量
 let imss=0;
 //下落速度
-let speed = 20;
+let speed = 30;
+//出现的个数
+let speeds=500;
+//关闭速度定时器
+let timesp=null;
+
+//创建自动改变速度的函数
+let speeded=()=>{
+    clearInterval(timesp)
+    timesp=setInterval(()=>{
+        speed --
+        speeds=speeds-10;
+        if(speed===3 || speeds===200){
+            speed===3
+            speeds===200
+            clearInterval(timesp)
+        }
+    },6000)
+}
+//一段时间生成一个dom的定时器
+let timenew;
+//节流锁
+let lock = true;
+//为保存开始时的时间
+let time=0;
+//结束弹出框函数
+let over=()=>{
+  $('olnum').className="olnums"
+  $('imte').style.display='block'
+  $('over').style.display='block'
+  clearInterval(timenew)
+  //关闭字母下落
+  arrLi.forEach((vla, i) => {
+      clearInterval(vla.timea)
+  })
+ times= Math.ceil((new Date().getTime()-time)/1000)
+ $('imte').innerText=`用时:${ times}秒`
+}
+
 //页面选择的速度
 //移入状态
-$('select').addEventListener('mouseenter',()=>{
-[...$('select').children].forEach((val,ind,arr)=>{
-val.style.display="block"
-val.addEventListener('click',()=>{
-    if(val.innerText=='慢'){
-        speed = 30
-        arr[1].innerText=val.innerText
+// $('select').addEventListener('mouseenter',()=>{
+// [...$('select').children].forEach((val,ind,arr)=>{
+// val.style.display="block"
+// val.addEventListener('click',()=>{
+//     if(val.innerText=='慢'){
+//         speed = 30
+//         arr[1].innerText=val.innerText
 
-    }else if(val.innerText=='中'){
-        arr[1].innerText=val.innerText
-        speed = 20
-    }else if(val.innerText=='快'){
-        arr[1].innerText=val.innerText
-        speed = 5
-    }else if(val.innerText=='特快'){
-        arr[1].innerText=val.innerText
-        speed =1
-    }
+//     }else if(val.innerText=='中'){
+//         arr[1].innerText=val.innerText
+//         speed = 20
+//     }else if(val.innerText=='快'){
+//         arr[1].innerText=val.innerText
+//         speed = 5
+//     }else if(val.innerText=='特快'){
+//         arr[1].innerText=val.innerText
+//         speed =1
+//     }
 
-})
-})
-})
+// })
+// })
+// })
 
 //转成大写并转成数组
 let ABC = (abc.toUpperCase().split(''))
@@ -50,15 +88,15 @@ function Fall() {
     //更新top
     this.upFall()
 }
-//选择速度下拉函数
-let selects=()=>{
-    [...$('select').children].forEach((val,ind)=>{
-        if(ind!=0 && ind!=1){
-            val.style.display="none"
-        }
-    })
-}
-selects();
+// //选择速度下拉函数
+// let selects=()=>{
+//     [...$('select').children].forEach((val,ind)=>{
+//         if(ind!=0 && ind!=1){
+//             val.style.display="none"
+//         }
+//     })
+// }
+// selects();
 
 //给Fall原型增加创建掉下来的元素函数
 Fall.prototype.fallInit = function () {
@@ -77,20 +115,18 @@ Fall.prototype.fallInit = function () {
 
 //下落移动
 Fall.prototype.upFall = function () {
+    clearInterval(this.timea)
     this.timea = setInterval(() => {
         this.a++
         this.lis.style.top = this.tops + this.a + 'px'
     }, speed)
 }
-//一段时间生成一个dom
-let timenew;
-//节流锁
-let lock = true;
+
 
 //移出状态
-$('select').addEventListener('mouseleave',()=>{
-    selects();
-})
+// $('select').addEventListener('mouseleave',()=>{
+//     selects();
+// })
 //下落函数
 function xia() {
     if (!lock) return
@@ -99,18 +135,19 @@ function xia() {
     timenew = setInterval(() => {
         let fall = new Fall()
         for (let arr = arrLi.length, i = 0; i < arr; i++) {
-                if(arrLi[i].a>=500){
-                    arrLi[i].lis.style.display='none'
+                if(arrLi[i].a>=530){
                     imss++
+                    if(imss==10){
+                        over()
+                    }
                     $('miss').innerText=`错过的数量:${imss}`
                     $('dot').removeChild(arrLi[i].lis)
                     arrLi.splice(i, 1)
                 }
-               
             }
             
     }, 500)
-    //创建记录错过的字母数量
+    
 
     setTimeout(() => {
         lock = true
@@ -121,31 +158,35 @@ function xia() {
 $('go').addEventListener('click', () => {
     $('go').style.display = 'none'
     xia()
+    //改变速度按钮
+    speeded()
+   time=new Date().getTime()
 
 })
-//暂停
-$('stop').addEventListener('click', () => {
-    clearInterval(timenew)
-    //关闭字母下落
-    arrLi.forEach((vla, i) => {
-        clearInterval(vla.timea)
-    })
 
+//结束
+$('stop').addEventListener('click', () => {
+   
+   over()
 })
 
 //继续
-$('goon').addEventListener('click', () => {
-    xia()
-    arrLi.forEach((vla, i) => {
-        clearInterval(vla.timea)
-        vla.timea = setInterval(() => {
-            vla.a++
-            vla.lis.style.top = vla.tops + vla.a + 'px'
-        }, speed)
+// $('goon').addEventListener('click', () => {
+//     $('olnum').className="s"
+//     $('imte').style.display='none'
+//     $('over').style.display='none'
 
-    })
+//     xia()
+//     arrLi.forEach((vla, i) => {
+//         clearInterval(vla.timea)
+//         vla.timea = setInterval(() => {
+//             vla.a++
+//             vla.lis.style.top = vla.tops + vla.a + 'px'
+//         }, speed)
 
-})
+//     })
+
+// })
 
 //重来
 restart.addEventListener('click', () => {
