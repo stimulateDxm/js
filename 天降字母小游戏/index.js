7de9be2f253ad9049fc757b89cec9f653c3c64c5
amain <99476728+stimulateDxm@users.noreply.github.com>
2022-04-17 11:45:59 +0800

    Add files via upload

--- 天降字母小游戏/index.js
+++ 天降字母小游戏/index.js
@@ -5,6 +5,34 @@ function $(ele) {
 
 //随机生成的字母
 let abc = "abcdefghijklnmopqrstuvwxyz"
+//记录错过的数量
+let imss=0;
+//下落速度
+let speed = 20;
+//页面选择的速度
+//移入状态
+$('select').addEventListener('mouseenter',()=>{
+[...$('select').children].forEach((val,ind,arr)=>{
+val.style.display="block"
+val.addEventListener('click',()=>{
+    if(val.innerText=='慢'){
+        speed = 30
+        arr[1].innerText=val.innerText
+
+    }else if(val.innerText=='中'){
+        arr[1].innerText=val.innerText
+        speed = 20
+    }else if(val.innerText=='快'){
+        arr[1].innerText=val.innerText
+        speed = 5
+    }else if(val.innerText=='特快'){
+        arr[1].innerText=val.innerText
+        speed =1
+    }
+
+})
+})
+})
 
 //转成大写并转成数组
 let ABC = (abc.toUpperCase().split(''))
@@ -22,6 +50,15 @@ function Fall() {
     //更新top
     this.upFall()
 }
+//选择速度下拉函数
+let selects=()=>{
+    [...$('select').children].forEach((val,ind)=>{
+        if(ind!=0 && ind!=1){
+            val.style.display="none"
+        }
+    })
+}
+selects();
 
 //给Fall原型增加创建掉下来的元素函数
 Fall.prototype.fallInit = function () {
@@ -35,47 +72,24 @@ Fall.prototype.fallInit = function () {
     this.lis.style.left = parseInt(Math.random() * 65) + "vw"
     this.tops = -50
     this.lis.style.top = this.tops + 'px'
+
 }
+
 //下落移动
 Fall.prototype.upFall = function () {
     this.timea = setInterval(() => {
         this.a++
         this.lis.style.top = this.tops + this.a + 'px'
-    }, 20)
-
+    }, speed)
 }
 //一段时间生成一个dom
 let timenew;
 //节流锁
 let lock = true;
-//下落速度
-let speed = 1000;
-//页面选择的速度
-//移入状态
-$('select').addEventListener('mouseenter',()=>{
-[...$('select').children].forEach((val,ind)=>{
-val.style.display="block"
-val.addEventListener('click',()=>{
-    if(val.innerText=='1秒出1个'){
-        speed = 1000
-    }else if(val.innerText=='1秒出2个'){
-        speed = 500
-    }else if(val.innerText=='1秒出3个'){
-        speed = 300
-    }else if(val.innerText=='1秒出4个'){
-        speed =250
-    }
-    
-})
-})
-})
+
 //移出状态
 $('select').addEventListener('mouseleave',()=>{
-    [...$('select').children].forEach((val,ind)=>{
-        if(ind!=0){
-            val.style.display="none"
-        }
-    })
+    selects();
 })
 //下落函数
 function xia() {
@@ -84,7 +98,20 @@ function xia() {
     clearInterval(timenew)
     timenew = setInterval(() => {
         let fall = new Fall()
-    }, speed)
+        for (let arr = arrLi.length, i = 0; i < arr; i++) {
+                if(arrLi[i].a>=500){
+                    arrLi[i].lis.style.display='none'
+                    imss++
+                    $('miss').innerText=`错过的数量:${imss}`
+                    $('dot').removeChild(arrLi[i].lis)
+                    arrLi.splice(i, 1)
+                }
+               
+            }
+            
+    }, 500)
+    //创建记录错过的字母数量
+
     setTimeout(() => {
         lock = true
     }, 200)
@@ -114,7 +141,7 @@ $('goon').addEventListener('click', () => {
         vla.timea = setInterval(() => {
             vla.a++
             vla.lis.style.top = vla.tops + vla.a + 'px'
-        }, 20)
+        }, speed)
 
     })
 
@@ -132,17 +159,17 @@ document.onkeydown = function (e) {
     // if(e.key.toUpperCase)
     let keys = e.key.toUpperCase()
     for (let arr = arrLi.length, i = 0; i < arr; i++) {
-        if (keys == arrLi[i].lis.innerText && arrLi[i].a >= 50 && arrLi[i].a <= 590) {
+        if (keys == arrLi[i].lis.innerText && arrLi[i].a >= 50 && arrLi[i].a <=500) {
             num++
             $('num').innerText = `完成的数量：${num}`
             $('dot').removeChild(arrLi[i].lis)
-
             arrLi.splice(i, 1)
+
         }
     }
 
-
 }
+
 
 
 
